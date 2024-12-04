ee66df90c0e89d8bdb0ec09d01e478ffbffc722a
Axel <lexa_f@mellifehra.de>
2024-12-04 19:43:27 +0100

    preparing for conda release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 # include=['spacerplacer', 'run_experiments', 'model', 'model.*']
 setup(
     name='spacerplacer',
-    version='1.1.0',
+    version='1.0.0',
     packages=find_packages(),
     package_data={'sp_model': ['mafft_scripts/mafft-linux64/*', 'mafft_scripts/mafft-mac/*', 'mafft_scripts/mafft-win/*',
                             'bdm_likelihood_computation/sympy_bdm_lh_fct/*'],
